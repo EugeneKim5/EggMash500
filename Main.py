794986c3b336154a5a1ed6d39efa6c58c3a1a9ed
Eugene Kim <eugenethesubmarine2424@gmail.com>
2026-05-18 11:58:53 -0500

    Test Change

--- Main.py
+++ Main.py
@@ -1,2 +1,8 @@
-print("chicken")
-print("chicken")
+print("Hello Human Beings, Please Enter your age: ")
+Age = int(input())
+if Age < 18:
+    print("You can not create an Account under the Rule of SEC")
+    print(f"The Current information we have are: {Age}")
+elif Age > 18:
+    Name = input("Enter Your Name: ")
+    print(f"The Current information we have are: {Name}, {Age}")
